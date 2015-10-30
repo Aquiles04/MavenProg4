448ffd6950775d9622f322429120fe08b3dc404c
alexandre.chaves <alexandre.chaves@LCET0918.cwbdis.pucpr.br>
2015-10-30 19:29:11 -0200

    Maven Aula Prog 4 30/outubro

---
+++ src/main/java/controladores/ProdutoControlador.java
@@ -0,0 +1,35 @@
+
+package controladores;
+
+import modelos.Produto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author alexandre.chaves
+ */
+@Controller
+public class ProdutoControlador {
+   
+   @RequestMapping("/produtos")
+   public ModelAndView obterProduto(){
+       
+       ModelAndView mav;
+       mav = new ModelAndView("listar-produtos");
+       
+       
+       Produto p;
+       p = new Produto();
+       p.setId(1L);
+       p.setNome("iphone");
+       p.setPrecoUnitario(9999.99f);
+       p.setQuantidade(1);
+       mav.addObject("produto",p);
+       
+       return mav;
+   }
+    
+    
+}
